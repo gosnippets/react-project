c50bbae91afc114637e18f328e96d443ccfe893a
Ramdas Gupta <er.rdgupta@gmail.com>
2022-11-30 16:44:23 +0530

    main

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Avatar from '@mui/material/Avatar';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container sx={{ m: '2rem' }}>
+      <Typography variant="h6" sx={{ mb: '15px', color: '#2196f3' }} gutterBottom>Create New Ticket</Typography>
+      <Grid container spacing={2} sx={{ mb: '15px' }}>
+        <Grid item xs={0.6}>
+          <Avatar sx={{ mt: '5px' }}>1</Avatar>
+        </Grid>
+        <Grid item xs={11.4}>
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Typography>Let us know about your ticket</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                malesuada lacus ex, sit amet blandit leo lobortis eget.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+        </Grid>
+      </Grid>
+
+      <Grid container spacing={2} sx={{ mb: '15px' }}>
+        <Grid item xs={0.6}>
+          <Avatar sx={{ mt: '5px' }}>2</Avatar>
+        </Grid>
+        <Grid item xs={11.4}>
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Typography>Detailed Information</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                malesuada lacus ex, sit amet blandit leo lobortis eget.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
