5741a842caab871cdf04863aec5a38729564fa38
Ramdas Gupta <er.rdgupta@gmail.com>
2022-11-30 19:06:06 +0530

    'form'

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,16 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Avatar from '@mui/material/Avatar';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
+import TextField from '@mui/material/TextField';
+import InputLabel from '@mui/material/InputLabel';
 
 function App() {
   return (
     <Container sx={{ m: '2rem' }}>
       <Typography variant="h6" sx={{ mb: '15px', color: '#2196f3' }} gutterBottom>Create New Ticket</Typography>
-      <Grid container spacing={2} sx={{ mb: '15px' }}>
+      <Grid container spacing={2} sx={{ mb: '15px' }} className='m-accordion'>
         <Grid item xs={0.6}>
-          <Avatar sx={{ mt: '5px' }}>1</Avatar>
+          <Avatar sx={{ mt: '5px' }} className='a-avatar'>1</Avatar>
         </Grid>
         <Grid item xs={11.4}>
           <Accordion>
@@ -22,21 +24,19 @@ function App() {
               aria-controls="panel1a-content"
               id="panel1a-header"
             >
-              <Typography>Let us know about your ticket</Typography>
+              <Typography className='a-typography'>Let us know about your ticket</Typography>
             </AccordionSummary>
             <AccordionDetails>
-              <Typography>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                malesuada lacus ex, sit amet blandit leo lobortis eget.
-              </Typography>
+              <InputLabel>What is your Request?</InputLabel>
+              <TextField required placeholder="Placeholder" variant="standard" />
             </AccordionDetails>
           </Accordion>
         </Grid>
       </Grid>
 
-      <Grid container spacing={2} sx={{ mb: '15px' }}>
+      <Grid container spacing={2} sx={{ mb: '15px' }} className='m-accordion'>
         <Grid item xs={0.6}>
-          <Avatar sx={{ mt: '5px' }}>2</Avatar>
+          <Avatar sx={{ mt: '5px' }} className='a-avatar'>2</Avatar>
         </Grid>
         <Grid item xs={11.4}>
           <Accordion>
@@ -45,7 +45,7 @@ function App() {
               aria-controls="panel1a-content"
               id="panel1a-header"
             >
-              <Typography>Detailed Information</Typography>
+              <Typography className='a-typography'>Detailed Information</Typography>
             </AccordionSummary>
             <AccordionDetails>
               <Typography>
