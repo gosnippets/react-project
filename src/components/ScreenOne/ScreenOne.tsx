cb6c56b5280b57fed3f7fd9f5a08b05243bf5d77
Ramdas Gupta <er.rdgupta@gmail.com>
2022-12-02 19:30:27 +0530

    'formik'

--- src/components/ScreenOne/ScreenOne.tsx
+++ src/components/ScreenOne/ScreenOne.tsx
@@ -1,20 +1,131 @@
+import * as React from 'react';
+import { useState } from 'react';
 import Typography from '@mui/material/Typography';
-import TextField from '@mui/material/TextField';
 import InputLabel from '@mui/material/InputLabel';
 import CustomAccordion from '../Common/CustomAccordion';
 import TicketForm from './TicketForm';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import MuiAlert, { AlertProps } from '@mui/material/Alert';
+import * as Yup from 'yup'
+import { useFormik } from 'formik'
+
+const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(props, ref) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
+const approvers = [
+    'Zain Press',
+    'Martin Septimus',
+    'Lydia Rhiel Madsen',
+    'Livia Culhane',
+    'Angel Dorwart',
+    'Livia Curtis',
+    'Tatiana Kenter'
+];
+
+const ticketSchema = Yup.object().shape({
+    title: Yup.string().min(10, 'Minimum 10 Symbols').max(200, 'Maximum 200 Symbols').required('Title is Required'),
+    description: Yup.string().min(30, 'Minimum 30 Symbols').max(500, 'Maximum 500 Symbols').required('Description is Required'),
+    ticketType: Yup.string().required('Type is Required'),
+    approver: Yup.string().required('Approver is Required'),
+    fromDate: Yup.date().required('From Date is Required'),
+    toDate: Yup.date().required('To Date is Required'),
+})
+
+const initialValues = {
+    title: '',
+    description: '',
+    ticketType: '',
+    approver: '',
+    fromDate: new Date(),
+    toDate: new Date(),
+}
+
 
 function ScreenOne() {
-    return (<>
-        <Typography variant="h6" sx={{ mb: '15px', color: '#2196f3' }} gutterBottom>Create New Ticket</Typography>
-        <CustomAccordion index='1' title='Let us know about your ticket'>
-            <TicketForm/>
-        </CustomAccordion>
+    const [approver, setApprover] = useState('');
+    const [loading, setLoading] = useState(false)
 
-        <CustomAccordion index='2' title='Detailed Information'>
-            <InputLabel sx={{ mb: '10px' }}>What is your Request?</InputLabel>
-            <TextField required placeholder="Enter Title" variant="standard" />
-        </CustomAccordion>
+    // const dispatch = useDispatch()
+    const formik = useFormik({
+        initialValues,
+        validationSchema: ticketSchema,
+        onSubmit: (values, { setStatus, setSubmitting }) => {
+            console.log('Values', values);
+            setLoading(true)
+            setTimeout(() => {
+                // login(values.email, values.password)
+                //     .then(({ data: { accessToken } }) => {
+                //         console.log("accessToken", accessToken);
+                //         setLoading(false)
+                //         dispatch(auth.actions.login(accessToken))
+                //     })
+                //     .catch(() => {
+                //         setLoading(false)
+                //         setSubmitting(false)
+                //         setStatus('The login detail is incorrect')
+                //     })
+            }, 1000)
+        },
+    })
+
+    const handleChange2 = (event: SelectChangeEvent) => {
+        // {...formik.getFieldProps('ticketType') }
+        console.log('event.target.value', event.target.value)
+        setApprover(event.target.value);
+        formik.setFieldValue("approver", event.target.value);
+    };
+
+    return (<>
+        <Typography variant="h6" sx={{ mb: '15px', color: '#2196f3' }}>Create New Ticket</Typography>
+        {formik.status && (
+            <div className='error-msg'>
+                <Alert severity="error">{formik.status}</Alert>
+            </div>
+        )}
+
+        <form id='ticket_form' onSubmit={formik.handleSubmit} noValidate>
+            <CustomAccordion index='1' title='Let us know about your ticket'>
+                <TicketForm formik={formik} />
+            </CustomAccordion>
+
+            <CustomAccordion index='2' title='Detailed Information'>
+                <InputLabel sx={{ mb: '10px' }} className='c-label'>Approver</InputLabel>
+                <FormControl sx={{ minWidth: 250 }} size="small">
+                    <Select
+                        displayEmpty
+                        value={approver}
+                        onChange={handleChange2}
+                        renderValue={(selected) => {
+                            if (selected.length === 0) {
+                                return <span className='menuitem-placeholder'>Select Approver's Name</span>;
+                            }
+                            return selected;
+                        }}>
+                        {approvers.map((approver) => (
+                            <MenuItem key={approver} value={approver}>{approver}</MenuItem>
+                        ))}
+                    </Select>
+                </FormControl>
+                {formik.touched.approver && formik.errors.approver && (
+                    <div className='message-container'>
+                        <span className='alert' role='alert'>{formik.errors.approver}</span>
+                    </div>
+                )}
+            </CustomAccordion>
+
+            <Stack spacing={2} direction="row" sx={{ mt: '35px', justifyContent: 'end' }}>
+                <Button type='reset' variant="text" disabled={formik.isSubmitting}>Cancel</Button>
+                <Button type='submit' variant="contained" disabled={formik.isSubmitting || !formik.isValid}>
+                    {!loading && <span>Submit</span>}
+                    {loading && (<span>Submitting... </span>)}
+                </Button>
+            </Stack>
+        </form>
     </>);
 }
 
--- src/components/ScreenOne/TicketForm.tsx
+++ src/components/ScreenOne/TicketForm.tsx
@@ -4,51 +4,107 @@ import TextareaAutosize from '@mui/material/TextareaAutosize';
 import Radio from '@mui/material/Radio';
 import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
-
-function TicketForm() {
-    return (
-        <form id='ticket_form'>
-            <InputLabel sx={{ mb: '10px' }} className='c-label'>What is your Request?</InputLabel>
-            <TextField required placeholder="Enter Title" variant="standard" />
-
-            <InputLabel sx={{ mt: '25px', mb: '10px' }} className='c-label'>Can you please write in brief about your Request?</InputLabel>
-            <TextareaAutosize required minRows={4} placeholder="Enter Description" className='c-textarea' />
-
-            <InputLabel sx={{ mt: '25px', mb: '10px' }} className='c-label'>Which type of Ticket would you like to Raise?</InputLabel>
-            <RadioGroup row name="row-radio-buttons-group">
-                <FormControlLabel value="Request" control={<Radio />} label="Request" className='radio-label'/>
-                <FormControlLabel value="Issue" control={<Radio />} label="Issue" className='radio-label'/>
-                <FormControlLabel value="Security Incident" control={<Radio />} label="Security Incident" className='radio-label'/>
-            </RadioGroup>
-
-            <InputLabel sx={{ mt: '25px', mb: '10px' }} className='c-label'>What kind of Request you want to proceed with?</InputLabel>
-            <RadioGroup row name="row-radio-buttons-group">
-                <FormControlLabel value="Hardware" control={<Radio />} label="Hardware" className='radio-label'/>
-                <FormControlLabel value="Software" control={<Radio />} label="Software" className='radio-label'/>
-                <FormControlLabel value="Network" control={<Radio />} label="Network" className='radio-label'/>
-                <FormControlLabel value="Access" control={<Radio />} label="Access" className='radio-label'/>
-            </RadioGroup>
-
-            <InputLabel sx={{ mt: '25px', mb: '10px' }} className='c-label'>Let us know the Priority.</InputLabel>
-            <RadioGroup row name="row-radio-buttons-group">
-                <FormControlLabel value="Critical" control={<Radio />} label="Critical" className='radio-label'/>
-                <FormControlLabel value="High" control={<Radio />} label="High" className='radio-label'/>
-                <FormControlLabel value="Medium" control={<Radio />} label="Medium" className='radio-label'/>
-                <FormControlLabel value="Low" control={<Radio />} label="Low" className='radio-label'/>
-            </RadioGroup>
-
-            <InputLabel sx={{ mt: '25px', mb: '10px' }} className='c-label'>What is the Timeline of your Request?</InputLabel>
-            <RadioGroup row name="row-radio-buttons-group">
-                <FormControlLabel value="Temporary" control={<Radio />} label="Temporary" className='radio-label'/>
-                <FormControlLabel value="Permanant" control={<Radio />} label="Permanant" className='radio-label'/>
-            </RadioGroup>
-
-            <InputLabel sx={{ mt: '25px', mb: '10px' }} className='c-label'>What is the Duration of your Request?</InputLabel>
-            <TextField required placeholder="Enter Title" variant="standard" />
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
 
 
-        </form>
-    );
+function TicketForm({ formik }: any) {
+
+    const handleChange = (newValue: Date | null) => {
+        console.log('Date', newValue);
+    };
+
+    return (<>
+        <InputLabel sx={{ mb: '10px' }} className='c-label'>What is your Request?</InputLabel>
+        <TextField required placeholder="Enter Title" variant="standard" {...formik.getFieldProps('title')} />
+        {formik.touched.title && formik.errors.title && (
+            <div className='message-container'>
+                <span className='alert' role='alert'>{formik.errors.title}</span>
+            </div>
+        )}
+
+        <InputLabel sx={{ mt: '25px', mb: '10px' }} className='c-label'>Can you please write in brief about your Request?</InputLabel>
+        <TextareaAutosize required minRows={4} placeholder="Enter Description" className='c-textarea' {...formik.getFieldProps('description')} />
+        {formik.touched.description && formik.errors.description && (
+            <div className='message-container'>
+                <span className='alert' role='alert'>{formik.errors.description}</span>
+            </div>
+        )}
+
+        <InputLabel sx={{ mt: '25px', mb: '10px' }} className='c-label'>Which type of Ticket would you like to Raise?</InputLabel>
+        <RadioGroup row name="row-radio-buttons-group" {...formik.getFieldProps('ticketType')}>
+            <FormControlLabel value="Request" control={<Radio />} label="Request" className='radio-label' />
+            <FormControlLabel value="Issue" control={<Radio />} label="Issue" className='radio-label' />
+            <FormControlLabel value="Security Incident" control={<Radio />} label="Security Incident" className='radio-label' />
+        </RadioGroup>
+        {formik.touched.ticketType && formik.errors.ticketType && (
+            <div className='message-container'>
+                <span className='alert' role='alert'>{formik.errors.ticketType}</span>
+            </div>
+        )}
+
+        <InputLabel sx={{ mt: '25px', mb: '10px' }} className='c-label'>What kind of Request you want to proceed with?</InputLabel>
+        <RadioGroup row name="row-radio-buttons-group">
+            <FormControlLabel value="Hardware" control={<Radio />} label="Hardware" className='radio-label' />
+            <FormControlLabel value="Software" control={<Radio />} label="Software" className='radio-label' />
+            <FormControlLabel value="Network" control={<Radio />} label="Network" className='radio-label' />
+            <FormControlLabel value="Access" control={<Radio />} label="Access" className='radio-label' />
+        </RadioGroup>
+
+        <InputLabel sx={{ mt: '25px', mb: '10px' }} className='c-label'>Let us know the Priority.</InputLabel>
+        <RadioGroup row name="row-radio-buttons-group">
+            <FormControlLabel value="Critical" control={<Radio />} label="Critical" className='radio-label' />
+            <FormControlLabel value="High" control={<Radio />} label="High" className='radio-label' />
+            <FormControlLabel value="Medium" control={<Radio />} label="Medium" className='radio-label' />
+            <FormControlLabel value="Low" control={<Radio />} label="Low" className='radio-label' />
+        </RadioGroup>
+
+        <InputLabel sx={{ mt: '25px', mb: '10px' }} className='c-label'>What is the Timeline of your Request?</InputLabel>
+        <RadioGroup row name="row-radio-buttons-group">
+            <FormControlLabel value="Temporary" control={<Radio />} label="Temporary" className='radio-label' />
+            <FormControlLabel value="Permanant" control={<Radio />} label="Permanant" className='radio-label' />
+        </RadioGroup>
+
+        <InputLabel sx={{ mt: '25px', mb: '10px' }} className='c-label'>What is the Duration of your Request?</InputLabel>
+
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <div className='date-container'>
+                <div className='date'>
+                    <DesktopDatePicker
+                        label="From Date"
+                        inputFormat="DD/MM/YYYY"
+                        onChange={handleChange}
+                        {...formik.getFieldProps('fromDate')}
+                        renderInput={(params) => <TextField {...params} variant="standard" />}
+                    />
+                    {formik.touched.fromDate && formik.errors.fromDate && (
+                        <div className='message-container'>
+                            <span className='alert' role='alert'>{formik.errors.fromDate}</span>
+                        </div>
+                    )}
+                </div>
+                <div className='date'>
+                    <DesktopDatePicker
+                        label="To Date"
+                        inputFormat="DD/MM/YYYY"
+                        className='date'
+                        onChange={handleChange}
+                        {...formik.getFieldProps('toDate')}
+                        renderInput={(params) => <TextField {...params} variant="standard" />}
+                    />
+                    {formik.touched.toDate && formik.errors.toDate && (
+                        <div className='message-container'>
+                            <span className='alert' role='alert'>{formik.errors.toDate}</span>
+                        </div>
+                    )}
+                </div>
+            </div>
+        </LocalizationProvider>
+
+
+
+    </>);
 }
 
 export default TicketForm;
