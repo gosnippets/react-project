998f01267e16a7489570094adfbd93f23b9f53fc
Ramdas Gupta <er.rdgupta@gmail.com>
2022-12-05 19:34:18 +0530

    'redux'

--- src/state/services/TicketService.ts
+++ src/state/services/TicketService.ts
@@ -1,10 +1,8 @@
 import axios from "./axios-common";
 import { TicketType } from "../types";
 
-const API_URL = process.env.BASE_URL_API || 'api'
-
 const createTicket = (data: TicketType) => {
-  return axios.post(API_URL + "/ticket/createTicket", data);
+  return axios.post("/ticket/createTicket", data);
 };
 
 export const ticketService = { createTicket }; 
--- src/state/services/axios-common.ts
+++ src/state/services/axios-common.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "http://10.65.0.105:8080/",
   headers: {
     "Content-type": "application/json"
   }
