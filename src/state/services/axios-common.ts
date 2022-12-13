30cfc386a60f4c296dc056700b6a91a50ad22a5a
Ramdas Gupta <er.rdgupta@gmail.com>
2022-12-13 23:14:07 +0530

    redux-toolkit

--- src/state/services/axios-common.ts
+++ src/state/services/axios-common.ts
@@ -2,7 +2,5 @@ import axios from "axios";
 
 export default axios.create({
   baseURL: "http://10.65.0.105:8080/",
-  headers: {
-    "Content-type": "application/json"
-  }
+  headers: { "Content-type": "application/json" }
 });
--- src/state/store.ts
+++ src/state/store.ts
@@ -1,16 +1,30 @@
-import { createStore, applyMiddleware } from 'redux';
+// import { createStore, applyMiddleware } from 'redux';
+// import thunkMiddleware from 'redux-thunk';
+// import { createLogger } from 'redux-logger';
+// import rootReducer from './reducers';
+
+// const loggerMiddleware = createLogger();
+
+// export const store = createStore(
+//   rootReducer,
+//   applyMiddleware(
+//     thunkMiddleware,
+//     loggerMiddleware
+//   )
+// );
+
+// export type RootStore = ReturnType<typeof rootReducer>
+
+import { configureStore } from '@reduxjs/toolkit'
 import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
 import rootReducer from './reducers';
 
 const loggerMiddleware = createLogger();
 
-export const store = createStore(
-  rootReducer,
-  applyMiddleware(
-    thunkMiddleware,
-    loggerMiddleware
-  )
-);
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: [thunkMiddleware, loggerMiddleware]
+})
 
 export type RootStore = ReturnType<typeof rootReducer>
